c0fe3ab42f5ae1945d6fffb7603f6ce211351068
qinghongyou <1917958423@qq.com>
2021-06-02 07:28:45 +0800

    ConfigurableApplicationContext 常用方法

--- boot-01-helloworld/src/main/java/com/xiao/boot/MainApplication.java
+++ boot-01-helloworld/src/main/java/com/xiao/boot/MainApplication.java
@@ -2,12 +2,24 @@ package com.xiao.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        // 1、返回我们的IOC 容器
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+
+        // 2、查看容器里面的组件
+        String[] names = run.getBeanDefinitionNames();
+        for (String name: names){
+            System.out.println("name:"+name);
+        }
+
+        int beanDefinitionCount = run.getBeanDefinitionCount();
+        System.out.println("beanDefinitionCount："+beanDefinitionCount);
+
     }
 }
 
