964327c9b4bc4ef016a1281c18540a1624826bba
qinghongyou <1917958423@qq.com>
2021-06-02 20:55:57 +0800

    Springboot 常用注解

---
+++ boot-01-helloworld/src/main/java/com/xiao/boot/bean/User.java
@@ -0,0 +1,49 @@
+package com.xiao.boot.bean;
+
+public class User {
+    protected String name;
+    protected int age;
+
+    Pet pet;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", pet=" + pet +
+                '}';
+    }
+}
