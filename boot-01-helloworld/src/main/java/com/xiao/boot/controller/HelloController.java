bb80749e45543da005e623351b7186291ab3b374
qinghongyou <1917958423@qq.com>
2021-06-02 08:05:24 +0800

    ConfigurableApplicationContext 常用方法

--- boot-01-helloworld/src/main/java/com/xiao/boot/controller/HelloController.java
+++ boot-01-helloworld/src/main/java/com/xiao/boot/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.xiao.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
-    public String handle01(){
-        return "Hello, Spring Boot 2!";
+    public String handle01(@RequestParam("name") String name){
+        return "Hello, Spring Boot 2!" + name;
     }
 }
 
