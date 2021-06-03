6845c0254c2a30a89c2721389ebae1c442e70799
qinghongyou <1917958423@qq.com>
2021-06-03 08:16:11 +0800

    Springboot web 静态资源文件

---
+++ boot-02-web/src/main/java/com/xiao/boot/Boot02WebApplication.java
@@ -0,0 +1,12 @@
+package com.xiao.boot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot02WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Boot02WebApplication.class, args);
+    }
+}
---
+++ boot-02-web/src/main/java/com/xiao/boot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.xiao.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/1.jpg")
+    public String hello(){
+        return "aaa";
+    }
+}
