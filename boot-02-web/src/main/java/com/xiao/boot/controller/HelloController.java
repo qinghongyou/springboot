5b6fe25eb33853e134cf9082035a36fce6087665
qinghongyou <1917958423@qq.com>
2021-06-05 22:11:06 +0800

    Springboot Rest 映射请求

--- boot-02-web/src/main/java/com/xiao/boot/controller/HelloController.java
+++ boot-02-web/src/main/java/com/xiao/boot/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.xiao.boot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -10,4 +9,29 @@ public class HelloController {
     public String hello(){
         return "aaa";
     }
+
+    @GetMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.GET)
+    public String getUser(){
+        return "GET-张三";
+    }
+
+    @PostMapping("/user")
+    //  @RequestMapping(value = "/user",method = RequestMethod.POST)
+    public String saveUser(){
+        return "POST-张三";
+    }
+
+    @PutMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.PUT)
+    public String putUser(){
+        return "PUT-张三";
+    }
+
+    @DeleteMapping("/user")
+    //@RequestMapping(value = "/user",method = RequestMethod.DELETE)
+    public String deleteUser(){
+        return "DELETE-张三";
+    }
+
 }
