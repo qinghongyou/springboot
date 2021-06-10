caed3514005a4e99c2720856191cbf410e984347
qinghongyou <1917958423@qq.com>
2021-06-10 08:04:34 +0800

    Springboot ReturnValueHandler原理

---
+++ boot-02-web/src/main/java/com/xiao/boot/controller/ResponseTestController.java
@@ -0,0 +1,24 @@
+package com.xiao.boot.controller;
+
+import com.xiao.boot.bean.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+public class ResponseTestController {
+
+    @ResponseBody  //利用返回值处理器里面的消息转换器进行处理
+    @GetMapping(value = "/test/person")
+    public Person getPerson(){
+        Person person = new Person();
+        person.setAge(28);
+        person.setBirth(new Date());
+        person.setUserName("zhangsan");
+        return person;
+    }
+
+}
+
