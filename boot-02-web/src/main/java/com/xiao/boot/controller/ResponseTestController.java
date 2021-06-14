89b51dd3909b070212d4cfee35c9fb4e20f6eb88
qinghongyou <1917958423@qq.com>
2021-06-15 07:47:11 +0800

    Springboot 自定义内容协商策略

--- boot-02-web/src/main/java/com/xiao/boot/controller/ResponseTestController.java
+++ boot-02-web/src/main/java/com/xiao/boot/controller/ResponseTestController.java
@@ -10,6 +10,17 @@ import java.util.Date;
 @Controller
 public class ResponseTestController {
 
+    /**
+     * 1、浏览器发请求直接返回 xml    [application/xml]        jacksonXmlConverter
+     * 2、如果是ajax请求 返回 json   [application/json]      jacksonJsonConverter
+     * 3、如果硅谷app发请求，返回自定义协议数据  [appliaction/x-guigu]   xxxxConverter
+     *          属性值1;属性值2;
+     *
+     * 步骤：
+     * 1、添加自定义的MessageConverter进系统底层
+     * 2、系统底层就会统计出所有MessageConverter能操作哪些类型
+     * 3、客户端内容协商 [guigu--->guigu]
+     */
     @ResponseBody  //利用返回值处理器里面的消息转换器进行处理
     @GetMapping(value = "/test/person")
     public Person getPerson(){
