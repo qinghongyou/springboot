abbb16af8f53f593faaaf5b08330c5128fb011e5
qinghongyou <1917958423@qq.com>
2021-06-15 22:47:36 +0800

    Springboot thymeleaf使用

---
+++ boot-02-web/src/main/java/com/xiao/boot/controller/ViewTestController.java
@@ -0,0 +1,18 @@
+package com.xiao.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewTestController {
+
+    @GetMapping("/msg")
+    public String hello(Model model){
+        //model中的数据会被放在请求域中 request.setAttribute("a",aa)
+        model.addAttribute("msg","一定要大力发展工业文化");
+        model.addAttribute("link","http://www.baidu.com");
+        return "success";
+    }
+}
+
