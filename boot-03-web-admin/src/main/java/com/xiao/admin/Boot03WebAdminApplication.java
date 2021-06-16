6700670707e105d8abbb31e015312112b3acce8e
qinghongyou <1917958423@qq.com>
2021-06-16 08:18:59 +0800

    Springboot 后台管理系统基本功能

---
+++ boot-03-web-admin/src/main/java/com/xiao/admin/Boot03WebAdminApplication.java
@@ -0,0 +1,14 @@
+package com.xiao.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot03WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot03WebAdminApplication.class, args);
+    }
+
+
+}
