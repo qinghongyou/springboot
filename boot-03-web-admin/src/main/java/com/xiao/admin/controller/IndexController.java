279c09a2d76444dff5a003247dcc9aa5a1d24ab6
qinghongyou <1917958423@qq.com>
2021-07-01 23:00:59 +0800

    Springboot 自定义方式整合druid数据源

--- boot-03-web-admin/src/main/java/com/xiao/admin/controller/IndexController.java
+++ boot-03-web-admin/src/main/java/com/xiao/admin/controller/IndexController.java
@@ -3,17 +3,30 @@ package com.xiao.admin.controller;
 
 import com.xiao.admin.bean.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
 public class IndexController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @ResponseBody
+    @GetMapping("/sql")
+    public String queryFromDb(){
+        Long aLong = jdbcTemplate.queryForObject("select count(*) from account_tbl", Long.class);
+        return aLong.toString();
+    }
 
     /**
      * 来登录页
