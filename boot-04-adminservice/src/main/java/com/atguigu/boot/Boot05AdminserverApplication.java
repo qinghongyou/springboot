33e2ff4b59710dbc669f2e41464843f3601323de
qinghongyou <1917958423@qq.com>
2021-07-09 07:46:25 +0800

    Springboot 指标监控-Boot Admin Server

---
+++ boot-04-adminservice/src/main/java/com/atguigu/boot/Boot05AdminserverApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.boot;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+
+@EnableAdminServer
+@SpringBootApplication
+public class Boot05AdminserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot05AdminserverApplication.class, args);
+    }
+
+}
