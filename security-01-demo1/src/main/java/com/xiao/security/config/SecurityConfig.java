167ad200d844833268107b6e62f87c59775c87f9
qinghongyou <1917958423@qq.com>
2021-07-13 08:08:20 +0800

    SpringSecurity web权限方案-用户名密码配置类实现

---
+++ security-01-demo1/src/main/java/com/xiao/security/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.xiao.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // 加密
+        String password = passwordEncoder.encode("123456"); // 密码
+        auth.inMemoryAuthentication().withUser("xiaoyou").password(password).roles("admin"); // 用户名、角色
+    }
+
+    @Bean
+    PasswordEncoder password() {
+        return new BCryptPasswordEncoder();
+    }
+}
